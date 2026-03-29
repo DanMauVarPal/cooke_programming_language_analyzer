22e56ad5ea226cb0b14dafc50d03a13b0a6d26cd
Daniel Mauricio Vargas Palomeque <var28790@ttu.edu>
2026-03-29 17:26:16 -0500

    Update Files

--- parser/parser.h
+++ parser/parser.h
@@ -1,9 +1,29 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-void expr();
-void term();
-void factor();
+void P();
+void S();
+void Spd();
+void Slr();
+void M();
+void C();
+void Clr();
+void A();
+void Alr();
+void R();
+void Rpd();
+void E();
+void Elr();
+void T();
+void Tlr();
+void F();
+void V();
+void X();
+void H();
+void I();
+void Ipd();
+void L();
+void D();
 
 extern int nextToken;
 #endif
